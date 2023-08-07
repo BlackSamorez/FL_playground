be16c076a2916ed0f392d402aca82388d95e0d50
Andrei Panferov <andrei@blacksamorez.ru>
2023-08-07 06:35:05 +0300

    RandK

--- src/MiniFL/compressor.py
+++ src/MiniFL/compressor.py
@@ -63,15 +63,38 @@ class TopKBiasedCompressor(Compressor):
         assert x.dtype.is_floating_point
 
         _, indexes = torch.topk(torch.abs(x.data), k=self.k, sorted=False)
-        masks = torch.zeros_like(x, dtype=torch.uint8)
-        masks[indexes] = 1
-
         values = x[indexes]
 
         return Message(
             data=(indexes, values),
             size=values.numel() * get_num_bits(values.dtype)
             + min(self.k * math.log2(x.numel()), (x.numel() - self.k) * math.log2(x.numel()), x.numel()),
+            metadata={"shape": x.shape},
+        )
+
+    def decompress(self, msg: Message) -> Collection[Tensor]:
+        indexes, values = msg.data
+        x = torch.zeros(msg.metadata["shape"], dtype=values.dtype, device=values.device)
+        x[indexes] = values
+        return (x,)
+
+
+class RandKCompressor(Compressor):
+    def __init__(self, k: int, seed=0):
+        self.k = k
+        self.generator = torch.Generator()
+        self.generator.manual_seed(seed)
+
+    def compress(self, data: Collection[Tensor]) -> Message:
+        assert len(data) == 1
+        x = data[0]
+
+        indexes = torch.randperm(x.numel(), generator=self.generator)[: self.k]
+        values = x[indexes]
+
+        return Message(
+            data=(indexes, values),
+            size=values.numel() * get_num_bits(values.dtype),
             metadata={"shape": x.shape},
         )
 

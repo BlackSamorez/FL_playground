fc0dfed3e566f8427c56d9fbf8e87fa7c7948d2e
Andrei Panferov <andrei@blacksamorez.ru>
2023-08-08 22:06:56 +0300

    MARINA better model handling

--- src/MiniFL/algorithms/marina.py
+++ src/MiniFL/algorithms/marina.py
@@ -7,6 +7,7 @@ from torch import Tensor, nn
 
 from MiniFL.communications import DataReceiver, DataSender, get_sender_receiver
 from MiniFL.compressors import Compressor, IdentityCompressor, PermKUnbiasedCompressor
+from MiniFL.fn import DifferentiableFn
 from MiniFL.utils import Flattener, add_grad_dict, get_grad_dict
 
 from .interfaces import Client, Master
@@ -20,9 +21,7 @@ class MarinaClient(Client):
     def __init__(
         self,
         # Task
-        data: Tuple[Tensor, Tensor],
-        model: nn.Module,
-        loss_fn,
+        fn: DifferentiableFn,
         # Communications
         data_sender: DataSender,
         data_receiver: DataReceiver,
@@ -32,11 +31,7 @@ class MarinaClient(Client):
         p: float,
         seed: int = 0,
     ):
-        self.data = data
-        self.model = model
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=gamma)
-        self.flattener = Flattener(shapes={k: v.shape for k, v in self.model.named_parameters()})
-        self.loss_fn = loss_fn
+        self.fn = fn
 
         self.data_sender = data_sender
         self.data_receiver = data_receiver
@@ -47,15 +42,15 @@ class MarinaClient(Client):
         self.generator = torch.Generator()
         self.generator.manual_seed(seed)
         self.p = p
+        self.gamma = gamma
+
+        self.grad_prev = None
 
     def prepare(self):
         # Init \nabla f_i(x^0)
-        self.optimizer.zero_grad()
-        loss = self.loss_fn(self.model(self.data[0]), self.data[1])
-        loss.backward()
-        self.flattened_prev_grad = self.flattener.flatten(get_grad_dict(self.model))
+        self.grad_prev = self.fn.get_flat_grad_estimate()
         # And send it to master
-        self.data_sender.send(self.identity_uplink_compressor.compress(self.flattened_prev_grad))
+        self.data_sender.send(self.identity_uplink_compressor.compress(self.grad_prev))
 
     def step(self) -> float:
         # Receive g^k from master and apply it
@@ -64,39 +59,29 @@ class MarinaClient(Client):
         return self.send_grad_get_loss_()
 
     def send_grad_get_loss_(self) -> float:
-        self.optimizer.zero_grad()
-        loss = self.loss_fn(self.model(self.data[0]), self.data[1])
-        loss.backward()
-
-        grad_dict = get_grad_dict(self.model)
-        flattened_grad = self.flattener.flatten(grad_dict)
+        flattened_grad = self.fn.get_flat_grad_estimate()
 
         c = get_c(self.generator, self.p)
         if c:
             msg = self.identity_uplink_compressor.compress(flattened_grad)
         else:
-            msg = self.uplink_compressor.compress(flattened_grad - self.flattened_prev_grad)
+            msg = self.uplink_compressor.compress(flattened_grad - self.grad_prev)
         self.data_sender.send(msg)
 
-        self.flattened_prev_grad = flattened_grad
-        return float(loss)
+        self.grad_prev = flattened_grad
+        return self.fn.get_value()
 
     def apply_global_step_(self):
         msg = self.data_receiver.recv()
-        grad_dict = self.flattener.unflatten(self.identity_downlink_compressor.decompress(msg))
-
-        self.optimizer.zero_grad()
-        add_grad_dict(self.model, grad_dict=grad_dict)
-        self.optimizer.step()
+        aggregated_grad_estimate = self.identity_downlink_compressor.decompress(msg)
+        self.fn.step(-aggregated_grad_estimate * self.gamma)
 
 
 class MarinaMaster(Master):
     def __init__(
         self,
         # Task
-        eval_data: Tuple[Tensor, Tensor],
-        model: nn.Module,
-        loss_fn,
+        fn: DifferentiableFn,
         # Communications
         data_senders: Collection[DataSender],
         data_receivers: Collection[DataReceiver],
@@ -106,11 +91,7 @@ class MarinaMaster(Master):
         p: float,
         seed: int = 0,
     ):
-        self.eval_data = eval_data
-        self.model = model
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=gamma)
-        self.flattener = Flattener(shapes={k: v.shape for k, v in self.model.named_parameters()})
-        self.loss_fn = loss_fn
+        self.fn = fn
 
         self.data_senders = data_senders
         self.data_receivers = data_receivers
@@ -121,6 +102,9 @@ class MarinaMaster(Master):
         self.generator = torch.Generator()
         self.generator.manual_seed(seed)
         self.p = p
+        self.gamma = gamma
+
+        self.g_prev = self.fn.zero_like_grad()
 
     def prepare(self):
         # Initialize g_0
@@ -129,65 +113,49 @@ class MarinaMaster(Master):
     def step(self) -> float:
         # Broadcast g_t to all workers
         for sender, compressor in zip(self.data_senders, self.downlink_compressors):
-            grad_dict = get_grad_dict(self.model)
-            msg = compressor.compress(self.flattener.flatten(grad_dict))
+            msg = compressor.compress(self.g_prev)
             sender.send(msg)
-        self.optimizer.step()
+        self.fn.step(-self.g_prev * self.gamma)
 
-        # g_{k+1} = g_k + \sum_{i=1}^n g_i^{k+1}
+        # g_{k+1} = \sum_{i=1}^n g_i^{k+1}
         c = get_c(self.generator, self.p)
         if c:
             self.process_full_grads_()
         else:
             self.process_compressed_shifts_()
 
-        return self.val_loss_()
-
-    def scale_grads_(self, scale: float):
-        for v in self.model.parameters():
-            v.grad *= scale
+        return self.fn.get_value()
 
     def process_full_grads_(self):
-        self.model.zero_grad()
+        self.g_prev = self.fn.zero_like_grad()
         for reciever, compressor in zip(self.data_receivers, self.identity_uplink_compressors):
             msg = reciever.recv()
-            grad_dict = self.flattener.unflatten(compressor.decompress(msg))
-            add_grad_dict(self.model, grad_dict=grad_dict)
-        self.scale_grads_(1 / len(self.data_senders))
+            self.g_prev += compressor.decompress(msg)
+        self.g_prev /= len(self.data_senders)
 
     def process_compressed_shifts_(self):
-        self.scale_grads_(len(self.data_senders))
+        self.g_prev *= len(self.data_senders)
         for receiver, compressor in zip(self.data_receivers, self.uplink_compressors):
             msg = receiver.recv()
-            grad_dict = self.flattener.unflatten(compressor.decompress(msg))
-            add_grad_dict(self.model, grad_dict=grad_dict)
-        self.scale_grads_(1 / len(self.data_senders))
-
-    def val_loss_(self) -> float:
-        with torch.no_grad():
-            return float(self.loss_fn(self.model(self.eval_data[0]), self.eval_data[1]))
+            self.g_prev += compressor.decompress(msg)
+        self.g_prev /= len(self.data_senders)
 
 
 def get_marina_master_and_clients(
-    clients_data: Collection[Collection[Tuple[Tensor, Tensor]]],
-    eval_data: Collection[Tuple[Tensor, Tensor]],
-    model: nn.Module,
-    loss_fn,
+    master_fn: DifferentiableFn,
+    client_fns: Collection[DifferentiableFn],
     compressors: Collection[Compressor],
     gamma: float,
     p: float,
     seed: int = 0,
 ) -> Tuple[MarinaMaster, Collection[MarinaClient]]:
-    num_clients = len(clients_data)
+    num_clients = len(client_fns)
 
     uplink_comms = [get_sender_receiver() for _ in range(num_clients)]
     downlink_comms = [get_sender_receiver() for _ in range(num_clients)]
-    client_models = [deepcopy(model) for _ in range(num_clients)]
 
     master = MarinaMaster(
-        eval_data=eval_data,
-        model=model,
-        loss_fn=loss_fn,
+        fn=master_fn,
         data_senders=[s for s, r in downlink_comms],
         data_receivers=[r for s, r in uplink_comms],
         uplink_compressors=compressors,
@@ -196,12 +164,9 @@ def get_marina_master_and_clients(
         seed=seed,
     )
 
-    clients = []
-    for i in range(num_clients):
-        client = MarinaClient(
-            data=clients_data[i],
-            model=client_models[i],
-            loss_fn=loss_fn,
+    clients = [
+        MarinaClient(
+            fn=client_fns[i],
             data_sender=uplink_comms[i][0],
             data_receiver=downlink_comms[i][1],
             uplink_compressor=compressors[i],
@@ -209,29 +174,26 @@ def get_marina_master_and_clients(
             p=p,
             seed=seed,
         )
-        clients.append(client)
+        for i in range(num_clients)
+    ]
 
     return master, clients
 
 
 def get_permk_marina_master_and_clients(
-    clients_data: Collection[Collection[Tuple[Tensor, Tensor]]],
-    eval_data: Collection[Tuple[Tensor, Tensor]],
-    model: nn.Module,
-    loss_fn,
+    master_fn: DifferentiableFn,
+    client_fns: Collection[DifferentiableFn],
     gamma: float,
     p: float,
     compressors_seed: int = 0,
     seed: int = 0,
 ) -> Tuple[MarinaMaster, Collection[MarinaClient]]:
     return get_marina_master_and_clients(
-        clients_data=clients_data,
-        eval_data=eval_data,
-        model=model,
-        loss_fn=loss_fn,
+        master_fn=master_fn,
+        client_fns=client_fns,
         compressors=[
-            PermKUnbiasedCompressor(rank=i, world_size=len(clients_data), seed=compressors_seed)
-            for i in range(len(clients_data))
+            PermKUnbiasedCompressor(rank=i, world_size=len(client_fns), seed=compressors_seed)
+            for i in range(len(client_fns))
         ],
         gamma=gamma,
         p=p,

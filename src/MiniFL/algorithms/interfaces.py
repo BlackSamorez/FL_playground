a7ea99cd457f94a54696e43ee56108f30163d84d
Andrei Panferov <andrei@blacksamorez.ru>
2023-08-13 06:47:46 +0300

    better algoritms running

--- src/MiniFL/algorithms/interfaces.py
+++ src/MiniFL/algorithms/interfaces.py
@@ -1,15 +1,13 @@
-import multiprocessing
 import sys
-import threading
 import time
 from abc import ABC, abstractmethod
 from concurrent.futures import ProcessPoolExecutor, as_completed
+from multiprocessing.pool import Pool, ThreadPool
 from queue import SimpleQueue
 from typing import Collection
 
 import torch
 from torch import FloatTensor
-from torch.multiprocessing import Queue as TorchQueue
 from tqdm import trange
 
 from MiniFL.communications import AggregatorReciever, AggregatorSender, BroadcastReceiver, BroadcastSender
@@ -39,90 +37,82 @@ class Master(ABC):
         pass
 
 
-def run_algorithm_with_threads(master: Master, clients: Collection[Client], num_steps: int):
-    num_clients = len(clients)
-    uplink_comms = [SimpleQueue() for _ in range(num_clients)]
-    downlink_comms = [SimpleQueue() for _ in range(num_clients)]
+def worker_process_(client, broadcasted_master_tensor):
+    return client.step(broadcasted_master_tensor)
 
-    def run_master_(steps: int, master: Master, metrics: list):
-        sum_worker_tensor = torch.empty_like(master.fn.zero_like_grad())
-        total_bits_received = 0
-        for _ in trange(steps):
-            sum_worker_tensor.zero_()
-            for comm in uplink_comms:
-                msg = comm.get()
-                sum_worker_tensor += msg.data
-                total_bits_received += msg.size
 
-            msg, master_metrics = master.step(sum_worker_tensor)
-            master_metrics.total_bits_received = total_bits_received
-            metrics.append(master_metrics)
-            for comm in downlink_comms:
-                comm.put(msg.data)
-
-    def run_client_(i: int, steps: int, client: Client):
-        broadcasted_master_tensor = torch.zeros_like(master.fn.zero_like_grad())
-        for _ in range(steps):
-            msg, client_metrics = client.step(broadcasted_master_tensor)
-            uplink_comms[i].put(msg)
-            msg = downlink_comms[i].get()
-            broadcasted_master_tensor = msg.data
-
+def run_algorithm_sequantially(master: Master, clients: Collection[Client], num_steps: int):
+    total_bits_uplink = 0
+    total_bits_downlink = 0
     master_metrics = []
 
-    client_threads = []
-    for i, client in enumerate(clients):
-        client_threads.append(threading.Thread(target=run_client_, args=(i, num_steps, client)))
-        client_threads[-1].start()
+    broadcasted_master_tensor = torch.zeros_like(master.fn.zero_like_grad())
+    sum_worker_tensors = torch.zeros_like(master.fn.zero_like_grad())
+    for step in trange(num_steps):
+        worker_results = [worker_process_(client, broadcasted_master_tensor) for client in clients]
+        sum_worker_tensors = sum(result[0].data for result in worker_results)
+        total_bits_uplink += sum(result[0].size for result in worker_results)
 
-    master_thread = threading.Thread(target=run_master_, args=(num_steps, master, master_metrics))
-    master_thread.start()
-
-    master_thread.join()
-    for t in client_threads:
-        t.join()
+        master_result = master.step(sum_worker_tensors)
+        broadcasted_master_tensor = master_result[0].data
+        total_bits_downlink += master_result[0].size
+        master_metrics_ = master_result[1]
+        master_metrics_.total_bits_received = total_bits_uplink
+        master_metrics_.total_bits_sent = total_bits_downlink
+        master_metrics.append(master_metrics_)
 
     return master_metrics
 
 
-def worker_process_(uplink_queue, downlink_queue, clients: Collection[Client], num_steps: int):
-    aggregator_sender = AggregatorSender(uplink_queue, clients[0].fn.zero_like_grad())
-    broadcast_receiver = BroadcastReceiver(downlink_queue)
-    broadcasted_master_tensor = torch.zeros_like(clients[0].fn.zero_like_grad())
-    for _ in range(num_steps):
-        for client in clients:
-            msg, client_metrics = client.step(broadcasted_master_tensor)
-            aggregator_sender.add(msg)
-        aggregator_sender.flush()
-        broadcasted_master_tensor = broadcast_receiver.recieve()
+def run_algorithm_with_threads(master: Master, clients: Collection[Client], num_steps: int, num_threads: int = 10):
+    total_bits_uplink = 0
+    total_bits_downlink = 0
+    master_metrics = []
+    with ThreadPool(num_threads) as pool:
+        broadcasted_master_tensor = torch.zeros_like(master.fn.zero_like_grad())
+        broadcasted_master_tensor.share_memory_()
+        sum_worker_tensors = torch.zeros_like(master.fn.zero_like_grad())
+        for step in trange(num_steps):
+            worker_results = pool.starmap(
+                worker_process_,
+                [(client, broadcasted_master_tensor) for client in clients],
+            )
+            sum_worker_tensors = sum(result[0].data for result in worker_results)
+            total_bits_uplink += sum(result[0].size for result in worker_results)
 
+            master_result = master.step(sum_worker_tensors)
+            broadcasted_master_tensor = master_result[0].data
+            total_bits_downlink += master_result[0].size
+            master_metrics_ = master_result[1]
+            master_metrics_.total_bits_received = total_bits_uplink
+            master_metrics_.total_bits_sent = total_bits_downlink
+            master_metrics.append(master_metrics_)
 
-def master_process_(uplink_queues, downlink_queues, master: Master, num_steps: int):
-    metrics = []
-    aggregator_receiver = AggregatorReciever(uplink_queues, master.fn.zero_like_grad())
-    broadcast_sender = BroadcastSender(downlink_queues)
-    sum_worker_tensor = torch.empty_like(master.fn.zero_like_grad())
-    for _ in trange(num_steps):
-        sum_worker_tensor = aggregator_receiver.recieve()
-        msg, master_metrics = master.step(sum_worker_tensor)
-        master_metrics.total_bits_received = aggregator_receiver.n_bits_passed
-        metrics.append(master_metrics)
-        broadcast_sender.broadcast(msg)
-    return metrics
+    return master_metrics
 
 
 def run_algorithm_with_processes(master: Master, clients: Collection[Client], num_steps: int, num_processes: int = 10):
-    with multiprocessing.Manager() as manager:
-        uplink_queues = [manager.Queue(1) for _ in range(num_processes)]
-        downlink_queues = [manager.Queue(1) for _ in range(num_processes)]
-
-        with multiprocessing.Pool(num_processes) as pool:
-            result = pool.starmap_async(
+    total_bits_uplink = 0
+    total_bits_downlink = 0
+    master_metrics = []
+    with Pool(num_processes) as pool:
+        broadcasted_master_tensor = torch.zeros_like(master.fn.zero_like_grad())
+        broadcasted_master_tensor.share_memory_()
+        sum_worker_tensors = torch.zeros_like(master.fn.zero_like_grad())
+        for step in trange(num_steps):
+            worker_results = pool.starmap(
                 worker_process_,
-                [
-                    (uplink_queues[i], downlink_queues[i], clients[i::num_processes], num_steps)
-                    for i in range(num_processes)
-                ],
+                [(client, broadcasted_master_tensor) for client in clients],
             )
+            sum_worker_tensors = sum(result[0].data for result in worker_results)
+            total_bits_uplink += sum(result[0].size for result in worker_results)
 
-            master_process_(uplink_queues, downlink_queues, master, num_steps)
+            master_result = master.step(sum_worker_tensors)
+            broadcasted_master_tensor = master_result[0].data
+            total_bits_downlink += master_result[0].size
+            master_metrics_ = master_result[1]
+            master_metrics_.total_bits_received = total_bits_uplink
+            master_metrics_.total_bits_sent = total_bits_downlink
+            master_metrics.append(master_metrics_)
+
+    return master_metrics
